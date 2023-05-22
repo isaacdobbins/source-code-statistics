541607bb598cb7b76ba59e747d1b41031538ad41
idobbins <isaac.dobbins@icloud.com>
2023-05-22 14:43:12 -0600

    add context object

--- build_dataset.py
+++ build_dataset.py
@@ -6,34 +6,56 @@ import sys
 import time
 import uuid
 
-token = ""
+class context:
+    token = ""
+    headers = {}
+    out_dir = ""
 
-headers = {}
+def prepare_args(context):
 
-out_dir = ""
+    context.token = sys.argv[1]
 
-def clone_repos(repos):
+    context.headers = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": "Bearer {}".format(context.token),
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+
+    context.out_dir = os.path.join(os.getcwd(), 'repos')
+
+def create_dir(dir):
+
+    if os.path.exists(dir):
+        shutil.rmtree(dir)
+
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+    if not os.path.exists(dir):
+        os.makedirs(dir)
+
+def clone_repos(repos, out_dir):
 
     for i in repos:
         out_name = "{}-{}".format(i['name'], uuid.uuid4())
         os.system('cd {} && git clone --depth 1 --single-branch {} {}'.format(out_dir, i['html_url'], out_name))
 
-def get_github_public_repos(pages):
+def get_github_public_repos(context, pages):
     
     url = "https://api.github.com/repositories?since={}"
 
-    r = requests.get(url.format(0), headers=headers)
+    r = requests.get(url.format(0), headers=context.headers)
     
     if r.status_code != 200:
         return
 
-    clone_repos(r.json())
+    clone_repos(r.json(), context.out_dir)
 
     total = 1;
 
     while 'url' in r.links['next'] and total < pages:
             
-        r = requests.get(r.links['next']['url'], headers=headers)
+        r = requests.get(r.links['next']['url'], headers=context.headers)
 
         clone_repos(r.json())
 
@@ -41,23 +63,10 @@ def get_github_public_repos(pages):
 
 if __name__ == "__main__":
     
-    token = sys.argv[1]
+    ctx = context()
 
-    headers = {
-        "Accept": "application/vnd.github+json",
-        "Authorization": "Bearer {}".format(token),
-        "X-GitHub-Api-Version": "2022-11-28",
-    }
+    prepare_args(ctx)
 
-    out_dir = os.path.join(os.getcwd(), 'repos')
-    
-    if os.path.exists(out_dir):
-        shutil.rmtree(out_dir)
+    create_dir(ctx.out_dir)
 
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
-
-    if not os.path.exists(out_dir):
-        os.makedirs(out_dir)
-
-    get_github_public_repos(pages = 1)
+    get_github_public_repos(ctx, pages = 1)
