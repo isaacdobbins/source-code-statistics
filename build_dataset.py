b5a4c5e511c4c32d9edc293c070e2e259c8ccfbf
idobbins <isaac.dobbins@icloud.com>
2023-05-23 23:04:58 -0600

    add C, C++ language detection

--- build_dataset.py
+++ build_dataset.py
@@ -6,22 +6,15 @@ import sys
 import time
 import uuid
 
-class context:
-    token = ""
-    headers = {}
-    out_dir = ""
+token = sys.argv[1]
 
-def prepare_args(context):
+headers = {
+    "Accept": "application/vnd.github+json",
+    "Authorization": "Bearer {}".format(token),
+    "X-GitHub-Api-Version": "2022-11-28",
+}
 
-    context.token = sys.argv[1]
-
-    context.headers = {
-        "Accept": "application/vnd.github+json",
-        "Authorization": "Bearer {}".format(context.token),
-        "X-GitHub-Api-Version": "2022-11-28",
-    }
-
-    context.out_dir = os.path.join(os.getcwd(), 'repos')
+out_dir = os.path.join(os.getcwd(), 'repos')
 
 def create_dir(dir):
 
@@ -34,28 +27,40 @@ def create_dir(dir):
     if not os.path.exists(dir):
         os.makedirs(dir)
 
-def clone_repos(repos, out_dir):
+def clone_repos(repos):
 
     for i in repos:
-        out_name = "{}-{}".format(i['name'], uuid.uuid4())
-        os.system('cd {} && git clone --depth 1 --single-branch {} {}'.format(out_dir, i['html_url'], out_name))
+        
+        if not i['languages_url']:
+            continue
 
-def get_github_public_repos(context, pages):
+        url = i['languages_url']
+
+        r = requests.get(url, headers=headers)
+        languages = r.json()
+
+        if not 'C' in languages:
+            continue
+        
+        out_name = "{}-{}".format(i['name'], uuid.uuid4())
+        os.system('cd {} && git clone --depth 1 --progress --single-branch {} {}'.format(out_dir, i['html_url'], out_name))
+
+def get_github_public_repos(pages):
     
     url = "https://api.github.com/repositories?since={}"
 
-    r = requests.get(url.format(0), headers=context.headers)
+    r = requests.get(url.format(0), headers=headers)
     
     if r.status_code != 200:
         return
 
-    clone_repos(r.json(), context.out_dir)
+    clone_repos(r.json())
 
     total = 1;
 
     while 'url' in r.links['next'] and total < pages:
-            
-        r = requests.get(r.links['next']['url'], headers=context.headers)
+
+        r = requests.get(r.links['next']['url'], headers=headers)
 
         clone_repos(r.json())
 
@@ -63,10 +68,6 @@ def get_github_public_repos(context, pages):
 
 if __name__ == "__main__":
     
-    ctx = context()
+    create_dir(out_dir)
 
-    prepare_args(ctx)
-
-    create_dir(ctx.out_dir)
-
-    get_github_public_repos(ctx, pages = 1)
+    get_github_public_repos(pages = 100)
